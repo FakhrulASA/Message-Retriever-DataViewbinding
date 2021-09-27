dd6de19dd56784e8844631f44a06b705255cd0e7
fakhrul <fakhrul.alam@btracsl.com>
2021-09-27 09:46:52 +0600

    -data/view binding added

--- app/src/main/java/com/fakhrulasa/messageretriever/MainActivity.kt
+++ app/src/main/java/com/fakhrulasa/messageretriever/MainActivity.kt
@@ -3,50 +3,55 @@ package com.fakhrulasa.messageretriever
 import android.app.Activity
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.fakhrulasa.messageretriever.databinding.ActivityMainBinding
 import com.google.android.gms.auth.api.phone.SmsRetriever
 import com.google.android.gms.auth.api.phone.SmsRetrieverClient
 import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
-    private val REQ_USER_CONSENT = 200
-    private lateinit var otpET:EditText
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var otpET: EditText
     private lateinit var otpBtn: Button
+    var enterOPP: String = "Enter the OTP"
     lateinit var smsBroadcastReceiver: SmsBroadcastReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         initViews()
         startSmartUserConsent()
     }
 
+
     private fun initViews() {
-        otpET=findViewById(R.id.tv_otp)
-        otpBtn=findViewById(R.id.btn_otp)
+        otpET = findViewById(R.id.tv_otp)
+        otpBtn = findViewById(R.id.btn_otp)
     }
+
     private fun startSmartUserConsent() {
         val client: SmsRetrieverClient = SmsRetriever.getClient(this)
         client.startSmsUserConsent(null)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQ_USER_CONSENT) {
-            if (resultCode == RESULT_OK && data != null) {
-                val message = data.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE)
+    private var resultLauncherOTP =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val message = result?.data?.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE)
                 getOtpFromMessage(message)
             }
         }
-    }
 
     private fun getOtpFromMessage(message: String?) {
         val otpPattern = Pattern.compile("(|^)\\d{6}")
         val matcher = otpPattern.matcher(message)
         if (matcher.find()) {
-            otpET.setText(matcher.group(0))
+            binding.tvOtp.setText(matcher.group(0))
         }
     }
 
@@ -55,7 +60,7 @@ class MainActivity : AppCompatActivity() {
         smsBroadcastReceiver.smsBroadcastReceiverListener =
             object : SmsBroadcastReceiver.SmsBroadcastReceiverListener {
                 override fun onSuccess(intent: Intent?) {
-                    startActivityForResult(intent, REQ_USER_CONSENT)
+                    resultLauncherOTP.launch(intent)
                 }
 
                 override fun onFailure() {}
